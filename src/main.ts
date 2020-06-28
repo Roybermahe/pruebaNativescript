851c41197594d71a20095a4eefbad9421d2d4e9b
Royber <roybermanjarrez@gmail.com>
2020-06-28 18:36:25 -0500

    ejemplo funcional

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
 import { AppModule } from "./app/app.module";
 
+import * as firebase  from "nativescript-plugin-firebase/app";
+
+firebase.initializeApp({
+    persist: false
+});
+
 // A traditional NativeScript application starts by initializing global objects,
 // setting up global CSS rules, creating, and navigating to the main page.
 // Angular applications need to take care of their own initialization:
